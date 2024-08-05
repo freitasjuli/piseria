fd309361434aa57821b27bf3bfc7f345c08490a4
arangurenjulian <arangurenjulian659@gmail.com>
2024-08-05 01:01:46 -0500

    actualizacion cartas

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,24 @@ function App() {
       <Navegacion></Navegacion>
       <Header></Header>
       <div className='divFicha'>
-        <Ficha></Ficha>
-        <Ficha></Ficha>
-        <Ficha></Ficha>
+        <Ficha
+          imagen="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+          nombre ="peperoni"
+          ingredientes="peperoni, queso, pasta de tomate, bocadillo"
+          precio="5000"
+        ></Ficha>
+        <Ficha
+          imagen="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
+          nombre ="española"
+          ingredientes="mozarella, gorgonzola, parmesano, provolone"
+          precio="10000"
+        ></Ficha>
+        <Ficha
+          imagen="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
+          nombre ="Napolitana"
+          ingredientes="jamon, oregano, tomate"
+          precio="15000"
+        ></Ficha>
         
       </div>
     </>
--- src/componets/Ficha.jsx
+++ src/componets/Ficha.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Ficha = ({nombre,ingredientes,precio,imagen}) => {
     return ( 
-        <div>
+        <div className='pizzaFicha'>
             <img src={imagen}></img>
             <h3>{nombre}</h3>
             <hr></hr>
+            <h4>Ingredientes:</h4>
             <p>{ingredientes}</p>
             <hr></hr>
-            <p>precio: {precio}</p>
+            <p>precio: ${precio}</p>
             <button>ver mas </button>
             <button>comprar</button>
         </div>
